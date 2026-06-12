d1e80ca399a11c0184060f1607fb7565b78ad35d
eli <eelihalperin@gmail.com>
2026-06-12 13:57:49 +0300

    create function - update book

--- database/book_db.py
+++ database/book_db.py
@@ -9,27 +9,35 @@ class BookDB:
 
     def create_book(self, data: dict):
         values = tuple(v for v in data.values())
-        print(values)
         sql_txt = f"INSERT INTO books ({self.fields})"
         sql_txt += "VALUES (%s, %s, %s, %s, %s)"
         with self.cursor(dictionary=True) as cursor:
             cursor.execute((sql_txt), (values))
         self.conn.commit()
-        print(200)
 
     def get_all_books(self):
         with self.cursor(dictionary=True) as cursor:
             cursor.execute("SELECT * FROM books")
             data = cursor.fetchall()
-        self.conn.close()
         return data
 
     def get_book_by_id(self, id: int):
         with self.cursor(dictionary=True) as cursor:
             cursor.execute("SELECT * FROM books WHERE id = %s", (id,))
             data = cursor.fetchall()
-        self.conn.close()
         return data
+
+    def update_book(self, id: int, data: dict):
+        values = tuple(v for v in data.values()) + (id,)
+        sql_txt = """
+                    UPDATE books
+                    SET title = %s, author = %s, genre = %s, is_available = %s, borrowed_by_member_id = %s
+                    WHERE id = %s
+                      """
+        with self.cursor() as cursor:
+            cursor.execute(sql_txt, values)
+            conn.commit()
+        print(200)
 
 
 book_db = BookDB(conn)
