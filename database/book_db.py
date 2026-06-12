63f4e3bc8a98a38aba3c20c0036b503d241851b9
eli <eelihalperin@gmail.com>
2026-06-12 13:28:45 +0300

    create function - get book by id

--- database/book_db.py
+++ database/book_db.py
@@ -1,20 +1,35 @@
 from database.db_connection import db_connection as conn
 
+
 class BookDB:
-    def __init__(self):
+    def __init__(self, conn):
         self.conn = conn
-        self.cursor = self.conn.cursor(dictionary=True)
-    
-    def create_book(self):
-        pass
+        self.cursor = self.conn.cursor
+        self.fields = "title, author, genre, is_available, borrowed_by_member_id"
+
+    def create_book(self, data: dict):
+        values = tuple(v for v in data.values())
+        print(values)
+        sql_txt = f"INSERT INTO books ({self.fields})"
+        sql_txt += "VALUES (%s, %s, %s, %s, %s)"
+        with self.cursor(dictionary=True) as cursor:
+            cursor.execute((sql_txt), (values))
+        self.conn.commit()
+        print(200)
 
     def get_all_books(self):
-        with self.cursor as cursor:
-            cursor.execute(
-                "SELECT * FROM books"
-            )
+        with self.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM books")
             data = cursor.fetchall()
         self.conn.close()
         return data
-    
-book_db = BookDB()
+
+    def get_book_by_id(self, id: int):
+        with self.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM books WHERE id = %s", (id,))
+            data = cursor.fetchall()
+        self.conn.close()
+        return data
+
+
+book_db = BookDB(conn)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from database.db_connection import create_tables
 from database.db_connection import db_connection as conn
+from database.book_db import book_db
+
 
 def main():
     create_tables(conn)
+    # book_db.create_book({"title": "hp", "author": "jkr", "genre": "Fiction", "is_available": True, "borrowed_by_member_id": 5})
+    # print(*book_db.get_all_books())
+    print(book_db.get_book_by_id(5))
+
 
 main()
